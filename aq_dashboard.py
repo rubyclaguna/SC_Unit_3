750c57b7371320604948b84a000b8f055203ed8d
Ruby Laguna <rubyclaguna@Rubys-MacBook-Pro.local>
2020-02-21 20:52:13 -0800

    app doesnt work

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -1,9 +1,33 @@
-from flask import Flask 
+from flask import Flask
+from openaq import OpenAQ
+from flask_sqlalchemy import SQLAlchemy
 
-APP = Flask(__name__)
+app = Flask(__name__)
+api = OpenAQ()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+DB = SQLAlchemy(app)
 
-@APP.route('/')
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return 'Record {} {}'.format(self.datetime, self.value)
+
+@app.route('/')
 def root():
-    """Base View."""
-    message = "hi"
-    return message 
+    data = Record.query.filter(Record.value>=10).all()
+    return str(data)
+
+@app.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    data = api.measurements(city='Los Angeles', parameter='pm25')
+    for entry in data[1]['results']:
+        record = Record(datetime=(str(entry['date']['utc'])), value=entry['value'])
+        DB.session.add(record)
+    DB.session.commit()
+    return 'Data refreshed!'
